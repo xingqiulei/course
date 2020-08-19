5ca9758a88e74c545a9d76364f22a02147ca7b6f
xingqiulei <794593500@qq.com>
2020-08-19 09:02:50 +0800

    1:新增gateway子模块，增加gateway依赖，并注册到注册中心

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping
+    @RequestMapping("/test")
     public String test(){
         return "success";
     }
