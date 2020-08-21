8194c47153d26aeab9933cfdeb08690c2d157795
xingqiulei <794593500@qq.com>
2020-08-21 21:54:24 +0800

    1:新建server模块

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,23 @@
+package com.course.system.domain;
+
+public class Test {
+    public String id ;
+    public String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
